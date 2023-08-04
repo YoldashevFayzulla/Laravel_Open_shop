8039b09627dc3d9678d0ab55f36a9ace53e88e79
YoldashevFayzulla <yoldashevfayzulla0@gmail.com>
2023-08-04 20:36:42 +0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('admin.category.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+//admin panel
+Route::middleware('auth')->group(function () {
+    Route::resource('/category',CategoryController::class);
+    Route::resource('/post',PostController::class);
+
+});
+
+
+
+
+
+//profile
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
